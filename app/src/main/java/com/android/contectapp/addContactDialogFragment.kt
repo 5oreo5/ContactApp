84bb2b072f692dd6e367dd934852e0efbe52e39a
somethinginthebox <ha862888@gmail.com>
2023-09-05 14:24:48 +0900

    [FEAT] : ⚙️유효성, 버튼 토스트 추가

--- app/src/main/java/com/android/contectapp/addContactDialogFragment.kt
+++ app/src/main/java/com/android/contectapp/addContactDialogFragment.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.content.Intent
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -22,14 +24,21 @@ private const val ARG_PARAM2 = "param2"
  * Use the [addContactDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class addContactDialogFragment : Fragment() {
+class addContactDialogFragment : Fragment(){
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var useTextWatcher: TextWatcher
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+
 
 
         //isCancelable = true
@@ -46,6 +55,70 @@ class addContactDialogFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_add_contact_dialog, container, false)
+        val saveBtn = view.findViewById<Button>(R.id.add_saveBtn)
+        val cancleBtn = view.findViewById<Button>(R.id.add_cancelBtn)
+        val editName = view.findViewById<EditText>(R.id.add_editName)
+        val editMobile = view.findViewById<EditText>(R.id.add_mobileEdit)
+        val editSpecial = view.findViewById<EditText>(R.id.add_specialEdit)
+        val editMail = view.findViewById<EditText>(R.id.add_mailEdit)
+
+        useTextWatcher = object : TextWatcher {
+            val maxLength = 15
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length ?: 0 > maxLength) {
+                    editName.error = "최대 $maxLength 글자까지 입력 가능합니다."
+                } else {
+                    editName.error = null // 이전에 설정된 오류 지우기.
+                }
+            }
+        }
+
+
+
+        saveBtn.setOnClickListener(){
+            // 이름, 번호, 담당 ,메일주소
+            val name = editName.text.toString()
+            val mobile = editMobile.text.toString()
+            val special = editSpecial.text.toString()
+            val mail = editMail.text.toString()
+
+            when {
+                (name.isEmpty()) -> {
+                    Toast.makeText(requireContext(),"아이디를 입력해주세요!",Toast.LENGTH_SHORT).show()
+                }
+                (mobile.isEmpty())->{
+                    Toast.makeText(requireContext(),"번호를 입력해주세요!",Toast.LENGTH_SHORT).show()
+                }
+                (special.isEmpty())->{
+                    Toast.makeText(requireContext(),"담당과를 입력해주세요!",Toast.LENGTH_SHORT).show()
+                }
+                (mail.isEmpty())->{
+                    Toast.makeText(requireContext(),"이메일을 입력해주세요!",Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(requireContext(), "회원가입 완료!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(requireContext(),DetailContactFragment::class.java)
+                    intent.putExtra("aName",name)
+                    intent.putExtra("aMobile",mobile)
+                    intent.putExtra("aSpecial",special)
+                    intent.putExtra("aMail",mail)
+
+
+                }
+
+            }
+
+        }
+
+        cancleBtn.setOnClickListener(){
+            DetailContactFragment()
+        }
 
 
         // Inflate the layout for this fragment
