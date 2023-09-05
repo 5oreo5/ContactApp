c6bd59761b34e26d27201ecb8853ec166df0bab4
minji-0420 <pongiii17@gmail.com>
2023-09-05 22:54:00 +0900

    update

--- app/src/main/java/com/android/contectapp/ContactListFragment.kt
+++ app/src/main/java/com/android/contectapp/ContactListFragment.kt
@@ -5,30 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.contectapp.databinding.FragmentContactListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ContactListFragment : Fragment(R.layout.fragment_contact_list) {
 
-
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ContactListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    val bundle = arguments
+    private lateinit var binding : FragmentContactListBinding
+    private lateinit var rv : RecyclerView
+    private lateinit var adapter : recyclerviewAdapter
+    private var items = NewListRepository.getNewList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -36,30 +26,44 @@ class ContactListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        parentFragment
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_list, container, false)
+        binding = FragmentContactListBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ContactListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rv = binding.recyclerview
+        rv.layoutManager = LinearLayoutManager(requireContext())
+
+        adapter = recyclerviewAdapter(items as MutableList<Item>)
+        rv.adapter = adapter
+
+        adapter.setOnItemClickListener(object : recyclerviewAdapter.OnItemClickListener {
+
+            override fun onItemClick(data: Item, position: Int) {
+                val image = data.image
+                val name = data.name
+                val nickname = data.nickname
+                val phone = data.phone
+                val specialist = data.specialist
+                val email = data.email
+                val event = data.event
+                val status = data.status
+
+                val detailContactFragment = DetailContactFragment()
+                val bundle = Bundle()
+                bundle.putInt("image", image)
+                bundle.putString("name", name)
+                bundle.putString("nickname", nickname)
+                bundle.putString("phone", phone)
+                bundle.putString("specialist", specialist)
+                bundle.putString("email", email)
+                bundle.putString("event", event)
+                bundle.putString("status", status)
+                detailContactFragment.arguments = bundle
             }
+        })
     }
+
 }
