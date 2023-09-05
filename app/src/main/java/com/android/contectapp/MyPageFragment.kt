70757b6083d39b7ebf83cca118a7621690d3146c
minji-0420 <pongiii17@gmail.com>
2023-09-05 11:24:07 +0900

    update

--- app/src/main/java/com/android/contectapp/MyPageFragment.kt
+++ app/src/main/java/com/android/contectapp/MyPageFragment.kt
@@ -5,55 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.android.contectapp.databinding.FragmentMyPageBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding : FragmentMyPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
-    }
+       binding = FragmentMyPageBinding.inflate(layoutInflater)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // 추후에 contactlistfragment에서 putstring(setOnItemClickListener)
+        arguments?.let {
+            val name = it.getString("name")
+            val nickname = it.getString("nickname")
+            val mobile = it.getString("mobile")
+            val specialist = it.getString("specialist")
+            val email = it.getString("email")
+            val biography = it.getString("biography")
+            val status = it.getString("status")
+            binding.mypageTvName.text = name
+            binding.mypageTvNickname.text = nickname
+            binding.mypageTvPhone.text = mobile
+            binding.mypageTvSpecialist.text = specialist
+            binding.mypageTvEmail.text = email
+            binding.mypageTvBiography.text = biography
+            binding.mypageTvStatus.text = status
+        }
+        return binding.root
     }
 }
